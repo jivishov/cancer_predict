dc98e920663c1f3767adf2d7bb8cb53a721cdef0
jivishov <97884055+jivishov@users.noreply.github.com>
2024-01-25 08:57:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import requests
 def get_clean_data():
   data = pd.read_csv("https://raw.githubusercontent.com/jivishov/cancer_predict/main/data/data.csv")
   
-  # data = data.drop(['Unnamed: 32', 'id'], axis=1)
+  data = data.drop(['Unnamed: 32', 'id'], axis=1)
   
   data['diagnosis'] = data['diagnosis'].map({ 'M': 1, 'B': 0 })
   
