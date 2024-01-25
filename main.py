89bb765f27204eb79279dca0aa29398f7a9fc363
jivishov <97884055+jivishov@users.noreply.github.com>
2024-01-24 21:42:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -139,8 +139,8 @@ def get_radar_chart(input_data):
 
 
 def add_predictions(input_data):
-  model = pickle.load(open("model/model.pkl", "rb"))
-  scaler = pickle.load(open("model/scaler.pkl", "rb"))
+  model = pickle.load(open("model.pkl", "rb"))
+  scaler = pickle.load(open("scaler.pkl", "rb"))
   
   input_array = np.array(list(input_data.values())).reshape(1, -1)
   
@@ -148,25 +148,25 @@ def add_predictions(input_data):
   
   prediction = model.predict(input_array_scaled)
   
-  st.subheader("Cell cluster prediction")
-  st.write("The cell cluster is:")
+  st.subheader("Hüceyrə klaster təxmini")
+  st.write("Hüceyrə klasteri:")
   
   if prediction[0] == 0:
-    st.write("<span class='diagnosis benign'>Benign</span>", unsafe_allow_html=True)
+    st.write("<span class='diagnosis benign'>Xoşxassəli</span>", unsafe_allow_html=True)
   else:
-    st.write("<span class='diagnosis malicious'>Malicious</span>", unsafe_allow_html=True)
+    st.write("<span class='diagnosis malicious'>Bədxassəli</span>", unsafe_allow_html=True)
     
   
-  st.write("Probability of being benign: ", model.predict_proba(input_array_scaled)[0][0])
-  st.write("Probability of being malicious: ", model.predict_proba(input_array_scaled)[0][1])
+  st.write("Xoşxassəli şişin olma ehtimalı: ", model.predict_proba(input_array_scaled)[0][0])
+  st.write("Bədxassəli şişin olma ehtimalı: ", model.predict_proba(input_array_scaled)[0][1])
   
-  st.write("This app can assist medical professionals in making a diagnosis, but should not be used as a substitute for a professional diagnosis.")
+  st.write("Bu tətbiq diaqnozun təyin edilməsində kömək edə bilər, lakin mütəxəssis rəyini əvəzləməməli.")
 
 
 
 def main():
   st.set_page_config(
-    page_title="Breast Cancer Predictor",
+    page_title="Sinə Xərçəngi Prediktoru",
     page_icon=":female-doctor:",
     layout="wide",
     initial_sidebar_state="expanded"
